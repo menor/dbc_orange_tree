9ef694ab1da93a5dd858e746d582df5e5fb7da3c
Phil Vargas & Matthew Knudsen <pair+PhilVargas.mknudsen01@devbootcamp.com>
2014-02-24 16:26:06 -0800

    implement release1 and 2.

--- source/javascripts/src/plain-old-js-objects.js
+++ source/javascripts/src/plain-old-js-objects.js
@@ -1,2 +1,49 @@
 /* Write your code for the plain-old-js-objects-spec here */
+function Tree(){
+  this.age = 0,
+  this.height = 0,
+  this.orangeCount = 0,
+  FRUIT_BEARING_AGE = 5,
+  MAX_AGE = 50,
+  this.isAlive = true
+};
+
+function Orange(){
+  this.diameter = Math.floor(Math.random()*5 + 1);
+}
+
+
+Tree.prototype = {
+  grow: function(){
+    this.age ++;
+    this.height += 10;
+    if (this.age >= FRUIT_BEARING_AGE) {
+      this.orangeCount += Math.floor(Math.random()*10 + 1);
+    }
+    this.die();
+  },
+
+  die: function(){
+    if (this.age > MAX_AGE){
+      this.isAlive = false;
+    }
+  },
+
+  dropOrange: function(){
+    this.orangeCount --
+    // pickOrange();
+    return new Orange();
+  }
+}
+
+function pickOrange(){
+  return new Orange();
+}
+
+function createTree() {
+  return new Tree();
+};
+
+
+
 
--- source/javascripts/src/typed-objects.js
+++ source/javascripts/src/typed-objects.js
@@ -1,1 +1,40 @@
-// Write your Orange Tree code here - you may use constructor functions 
+// Write your Orange Tree code here - you may use constructor functions
+function OrangeTree(){
+  this.age = 0,
+  this.height = 0,
+  this.orangeCount = 0,
+  FRUIT_BEARING_AGE = 5,
+  MAX_AGE = 50,
+  this.isAlive = true
+};
+
+function Orange(){
+  this.diameter = Math.floor(Math.random()*5 + 1);
+}
+
+function pickOrange(){
+  return new Orange();
+}
+
+OrangeTree.prototype = {
+  grow: function(){
+    this.age ++;
+    this.height += 10;
+    if (this.age >= FRUIT_BEARING_AGE) {
+      this.orangeCount += Math.floor(Math.random()*10 + 1);
+    }
+    this.die();
+  },
+
+  die: function(){
+    if (this.age > MAX_AGE){
+      this.isAlive = false;
+    }
+  },
+
+  dropOrange: function(){
+    this.orangeCount --
+    // pickOrange();
+    return new Orange();
+  }
+}
