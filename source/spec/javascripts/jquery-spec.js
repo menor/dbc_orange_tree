834d90ce171083c9b5dad77cb154bd05e09ed2a9
Brittany Mazza & José Menor <pair+LadyMozzarella.menor@devbootcamp.com>
2014-02-24 19:56:19 -0800

    Attempt to implement Jasmine tests.

--- source/spec/javascripts/jquery-spec.js
+++ source/spec/javascripts/jquery-spec.js
@@ -1,1 +1,14 @@
-// Write your jquery specs here.
+// We tried!!! :(
+
+// describe("funtionCalled", function() {
+//    it("calls the click() function", function() {
+//       $('.plant').click();
+//       expect('click').toHaveBeenTriggeredOn($('.plant'));
+
+
+
+//           // spy = jasmine.createSpy("createTree()");
+//           // $( '.plant' ).click();
+//           // expect(spy).toHaveBeenCalled();
+//       });
+// });
