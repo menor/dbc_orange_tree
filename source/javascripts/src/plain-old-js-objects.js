410d5c8385c3f2f9d82d92cbc65896e2025066c3
Chris Woodrich & Irene Yiu <pair+chriswoodrich.ireneyiu@devbootcamp.com>
2014-02-24 17:00:56 -0800

    Release 1, plain old JS

--- source/javascripts/src/plain-old-js-objects.js
+++ source/javascripts/src/plain-old-js-objects.js
@@ -1,2 +1,36 @@
 /* Write your code for the plain-old-js-objects-spec here */
+var FRUIT_BEARING_AGE = 15;
+var MAX_AGE = 15;
+var createTree = function(){
+  return {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    isAlive: true,
+    grow: function() {
+      this.age +=1, this.height += 10
+      if(this.age>=FRUIT_BEARING_AGE){
+        this.orangeCount = Math.floor(Math.random()*100)
+      }
+      if(this.age>MAX_AGE) {
+        this.isAlive = false
+      }
+    },
+    dropOrange: function(){
+      return this.orangeCount--
+    }
+  }
+};
+
+function Orange() {
+  this.diameter = Math.floor((Math.random())+3);
+}
+
+function pickOrange() {
+  return new Orange();
+}
+
+
+
+tree = createTree();
 
