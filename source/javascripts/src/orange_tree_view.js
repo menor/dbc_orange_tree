fd85d6e6f324d4b016573c6a72f92cf64a2269fb
Darren Dahl & Henry Hsu <pair+darrendahl.henryh28@devbootcamp.com>
2014-02-24 19:42:48 -0800

    added some more functionality, trees can die and can tell you when you picked all the oranges

---
+++ source/javascripts/src/orange_tree_view.js
@@ -0,0 +1,2 @@
+var OrangeTreeView = function(){
+}
--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -1,17 +1,29 @@
 /* Write your JS to modify the view here */
+
+
 $(document).ready(function(){
   $('.plant').on('click', function(){
-
     tree = new Tree();
 
     if($('#orange-tree-template').is(':hidden')){
+      $('.tree_dead').html("")
       $('#orange-tree-template').toggle();
     }
-    // $('.display-tree-big').append('<img id="orange-tree" src="images/tree.jpeg"/>')
   })
 
 
   $('.age').on('click', function(){
+
+    if(tree.age == MAX_AGE){
+
+      $('img').remove('#orange')
+      $('p.fruit-count').html(0)
+      $('p.age').html(0)
+      $('#orange-tree-template').toggle();
+      $('.tree_dead').html("YOUR TREE DEADDDD")
+      return
+    }
+
     tree.grow();
 
     $('p.age').html(tree.age)
@@ -24,18 +36,20 @@ $(document).ready(function(){
       }
     }
 
+    $('p.fruit-count').html(tree.orangeCount)
+
   })
 
   $('.pick').on('click', function(){
+
+    if(tree.orangeCount <= 0){
+      $('p.fruit-count').html("No More Oranges!");
+      return;
+    }
+
       $('.display-tree-big:first-child').remove()
       tree.dropOrange();
-
-      $('img').remove('#orange')
-
-       for(var i=0; i < tree.orangeCount; i++){
-        $('.display-tree-big').append('<img id="orange" src="images/orange.gif"/>')
-      }
-
+      $('#orange:first-child').remove();
       $('p.fruit-count').html(tree.orangeCount)
   })
 
