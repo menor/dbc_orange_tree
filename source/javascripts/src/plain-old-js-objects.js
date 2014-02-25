fae280454bff0c27cd5a7b2923e5bf4319a015e0
Armando & Tim <pair+trekkie4life.tyau@devbootcamp.com>
2014-02-24 18:25:27 -0800

    Got a lot of view.js to work; got plant, grow, and age tree to work upon button click.

--- source/javascripts/src/plain-old-js-objects.js
+++ source/javascripts/src/plain-old-js-objects.js
@@ -1,15 +1,14 @@
 /* Write your code for the plain-old-js-objects-spec here */
 
+//TREE RELATES STUFF
 function Tree() {
-  FRUIT_BEARING_AGE= 6; //this.FRUIT_BEARING_AGE = 6; //works anywhere EXCEPT in the prototype
+  FRUIT_BEARING_AGE = 6; //this.FRUIT_BEARING_AGE = 6; //works anywhere EXCEPT in the prototype
   MAX_AGE = 23;
-
 
   this.age = 0;
   this.height = 0;
   this.orangeCount = 0;
   this.isAlive = true;
-
 }
 
 var createTree = function () {
@@ -18,18 +17,18 @@ var createTree = function () {
 
 
 Tree.prototype = {
-
   //grow
   grow: function() {
-    this.age += 1;
-    this.height += 10;
+    if (this.age <= MAX_AGE) {
+      this.age += 1;
+      this.height += 10;
 
-    if (this.age >= FRUIT_BEARING_AGE) {
-      this.orangeCount = Math.floor((Math.random() * 12) + 2);
-    }
+      if (this.age >= FRUIT_BEARING_AGE) {
+        this.orangeCount += Math.floor((Math.random() * 12) + 1);
+      }
 
-    if (this.age > MAX_AGE){
-      this.isAlive = false
+    } else {
+      this.isAlive = false;
     }
   },
 
@@ -37,15 +36,18 @@ Tree.prototype = {
   dropOrange: function() {
     var orangesDropped = 0;
     if (this.age >= FRUIT_BEARING_AGE && this.orangeCount > 0) {
-      orangesDropped = Math.floor((Math.random() * 12) + 2);
+      orangesDropped = Math.floor((Math.random() * 12) + 1);  //this.orangeCount
     }
     this.orangeCount -= orangesDropped;
     return orangesDropped;
   }
-
 }
 
 
+
+
+
+//ORANGE RELATED STUFF
 function Orange() {
   this.diameter = Math.floor((Math.random() * 10) + 1);
 }
--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -1,1 +1,39 @@
 /* Write your JS to modify the view here */
+
+var tree = createTree();
+
+window.onload = function() {
+
+
+
+  $(".plant").on("click", function() {
+      $("#orange-tree-template").css("display", "block")
+      $("p.age").html("Age: " + tree.age);
+      $("p.fruit-count").html("Fruit Count: " + tree.orangeCount);
+
+    });
+
+  $(".pick").on("click", function() {
+
+  });
+
+
+
+
+
+
+
+
+
+  $(".age").on("click", function() {
+    tree.grow();
+    if (tree.isAlive) {
+      $("p.age").html("Age: " + tree.age);
+      $("p.fruit-count").html("Fruit Count: " + tree.orangeCount);
+    } else {
+      $("p.age").html("I am Dead, wasted into nothing, damn you cruel world. Damn you.");
+      $("p.fruit-count").html("Eat this fruit and die a painful diahrrea filled death.");
+    }
+  });
+
+}
