c8ea1992afb8136bb28ad4b1a2f8ff5e8f565cb1
Harry Whelchel <harrywhelchel@gmail.com>
2014-02-24 19:52:52 -0800

    Extends killTree animations using JQuery scale effects.

--- source/javascripts/src/typed-objects.js
+++ source/javascripts/src/typed-objects.js
@@ -1,14 +1,14 @@
 // Write your Orange Tree code here - you may use constructor functions
 
-var FRUIT_BEARING_AGE = 10
-var MAX_AGE = 20
+var FRUIT_BEARING_AGE = 3;
+var MAX_AGE = 6;
 
 function Tree(){
-  this.age = 0
-  this.height = 0
-  this.orangeCount = 0
-  this.isAlive = true
-};
+  this.age = 0;
+  this.height = 0;
+  this.orangeCount = 0;
+  this.isAlive = true;
+}
 
 function Orange(){
   this.diameter = Math.random()*(10);
@@ -20,23 +20,23 @@ function pickOrange(){
 
 Tree.prototype = {
   grow: function() {
-    this.age += 1
-    this.height += 10
+    this.age += 1;
+    this.height += 10;
     if (this.age >= FRUIT_BEARING_AGE)
     {
       this.orangeCount = Math.random()*(10);
     }
     if (this.age > MAX_AGE)
     {
-      this.isAlive = false
+      this.isAlive = false;
     }
   },
   dropOrange: function() {
-    this.orangeCount -= 1
-    return 1
+    this.orangeCount -= 1;
+    return 1;
   },
 };
 
 function createTree() {
   return new Tree();
-};
+}
--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -4,23 +4,30 @@ var JQueryWrapper = {
   plantTree: function() {
     $("#orange-tree-template").css({"display":"inline"});
     tree = createTree();
-    $("h2").html("You made a tree!")
+    $("h2").html("You made a tree!");
   },
   ageTree: function() {
     tree.grow();
     $('p.age').html(tree.age);
     if (tree.age > MAX_AGE)
     {
-      $('.display-tree-big').css({"visibility":"hidden"})
-      $('p.age').html('Dead')
+      JQueryWrapper.killTree();
     }
     else if (tree.age >= FRUIT_BEARING_AGE)
     {
-    $('.display-tree-big').prepend('<img src="../source/images/orange.gif" />')
+    $('.display-tree-big').prepend('<img src="../source/images/orange.gif" />');
     }
+  },
+  killTree: function() {
+    $('.display-tree-big').toggle({ effect:"scale", direction:"both", origin:["top","left"]});
+      $('.display-tree-big').addClass('display-tree-small').removeClass('display-tree-big');
+      $('p.age').html('Dead');
+      $("h2").html("Your tree withered and died!");
+      $('.display-tree-small').fadeIn('slow',function(){
+        $('.display-tree-small').fadeOut('fast');
+      });
   }
-}
-
+};
 
 
 $(function () {
