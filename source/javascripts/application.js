d35007b784bf12b106844919e77c0428b03a91e6
Steven G. Harms <steven@devbootcamp.com>
2014-02-26 06:20:48 -0800

    Extract eventingSelectors object
    It's handy for both the `Binder` and the `View` to
    know about this collection of classes.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,14 +1,18 @@
 $(function() {
+  var eventingSelectors = {
+    plantActuatorSelector: "button.plant",
+    pickActuatorSelector: "button.pick",
+    ageActuatorSelector: "button.age"
+  };
+
   OrangesApp.view = new OrangesApp.View({
     treeDivSelector: "#orange-tree-template",
     ageDisplaySelector: "p.age",
     fruitCountDisplaySelector: "p.fruit-count",
     orangesImagesContainer: ".display-tree-big div"
   });
+
   OrangesApp.controller = new OrangesApp.Controller({ view: OrangesApp.view });
-  new OrangesApp.Binder({
-    plantActuatorSelector: "button.plant",
-    pickActuatorSelector: "button.pick",
-    ageActuatorSelector: "button.age"
-  }, OrangesApp.controller).bind();
+  new OrangesApp.Binder(eventingSelectors, OrangesApp.controller).bind();
+  OrangesApp.view.eventingSelectors = eventingSelectors;
 });
