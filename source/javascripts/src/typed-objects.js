6aa7f23d52a16064f4b96590afa6c1e2a56be3f8
Steven G. Harms <steven@devbootcamp.com>
2014-02-26 03:52:21 -0800

    Refactor: Move Classes to appropriate file
    Rehome classes into a file that makes sense to
    their design.

--- source/javascripts/src/typed-objects.js
+++ source/javascripts/src/typed-objects.js
@@ -1,1 +1,46 @@
-// Write your Orange Tree code here - you may use constructor functions 
+FRUIT_BEARING_AGE = 3;
+MAX_AGE = 10;
+
+function Tree() {
+  this.age = 0;
+  this.height = 0;
+  this.orangeCount = 0;
+  this.oranges = new Array();
+  this.isAlive = true;
+}
+
+function Orange() {
+  this.diameter = Math.floor((Math.random()*10)+1);
+}
+
+Tree.prototype = {
+  grow: function() {
+    this.age++;
+    this.height += 10;
+    if(this.age == FRUIT_BEARING_AGE) {
+      this.orangeCount = Math.floor((Math.random()*10)+1);
+      for(var i=0; i<this.orangeCount; i++) {
+        this.oranges.push(new Orange);
+      }
+    }
+    if(this.age > MAX_AGE) {
+      this.isAlive = false;
+    }
+  },
+
+  dropOrange: function() {
+    if(this.orangeCount > 0) {
+      this.orangeCount--;
+      return new Orange;
+    }
+  }
+}
+
+function pickOrange(tree) {
+  tree.orangeCount--;
+  return tree.oranges.shift();
+}
+
+function createTree(){
+  return new Tree();
+};
