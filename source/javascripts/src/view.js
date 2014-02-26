877459079ab07cc422baaac759c398dd288ef2f9
Steven G. Harms <steven@devbootcamp.com>
2014-02-26 04:10:42 -0800

    Define OrangesApp.View.update
    This method takes a data-bearing something (duck
    type) that can be asked for a tree.
    
    After page load I can:
    
    ```javascript
    OrangesApp.view.update({tree: true});
    ```
    
    and see in the console
    
    ```
    OrangesApp.view.update({tree: true});
    i should display a tree!
    ```
    
    Next move: instead of `console.log()` some noise,
    let's activate the div.
    
    and see some output.

--- source/javascripts/src/view.js
+++ source/javascripts/src/view.js
@@ -5,4 +5,12 @@ $(function() {
 
 OrangesApp.View = function() {
   console.log('created a view, yay');
-}
+};
+
+OrangesApp.View.prototype = {
+  update: function(dataSource) {
+            if (dataSource.tree) {
+              console.log('i should display a tree!');
+            }
+          }
+};
